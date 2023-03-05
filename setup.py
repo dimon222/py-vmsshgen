0306384b9e4e26bc25950624165ddf1194991641
Dmitry Romanenko <Dmitry@Romanenko.in>
2023-03-05 13:14:44 -0500

    Bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="vmsshgen",
-    version="1.0.0",
+    version="1.0.1",
     author="Dmitry Romanenko",
     author_email="Dmitry@Romanenko.in",
     description="Automatic generation of SSH keys for VM",
